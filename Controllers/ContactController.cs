113c67f93e8614af3341b05dd9f65e96a74c3d59
Gaetanerk <gaetan.turben@outlook.com>
2025-01-13 16:06:48 +0100

    exercice1 asp

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercice_1.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact/AllContacts
+        public IActionResult AllContacts()
+        {
+            return View();
+        }
+
+        // GET: Contact/Contact
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        // GET: Contact/Contact/CreateContact
+        public IActionResult CreateContact()
+        {
+            return View();
+        }
+
+        }
+}
